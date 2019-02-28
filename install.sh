4990a4a68504e4afb912650323ce788a21894d6b
LukeSimons <lukemsimons@gmail.com>
2019-02-28 14:45:17 +0000

    Updated install files again

---
+++ install.sh
@@ -0,0 +1,17 @@
+# Install dependenceis: ZLIB, HDF5, NETCDF
+mkdir Data
+cd Dependencies
+
+. install_netcdf.sh
+
+# Get config4cpp for configuration
+git clone https://github.com/config4star/config4cpp.git
+
+#build config 4 cpp
+cd config4cpp
+make
+cd ../..
+
+# Install Boost libraries
+#echo "Installing boost libraries"
+#sudo apt-get install libboost-all-dev
