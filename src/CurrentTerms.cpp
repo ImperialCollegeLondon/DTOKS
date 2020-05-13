9fac0ca4533db3bf44fb57e6ffc88450601efb39
LukeSimons <lukemsimons@gmail.com>
2020-05-13 17:30:18 +0100

    Updated THSi and THSe Current Terms to match current theoretical model

--- src/CurrentTerms.cpp
+++ src/CurrentTerms.cpp
@@ -35,62 +35,32 @@ namespace Term{
         return Flux::DeltaSec(Sample,Pdata);
     }
     double THSe::Evaluate(const Matter* Sample, const std::shared_ptr<PlasmaData> Pdata, const double Potential){
-
         double TiTe = Pdata->IonTemp/Pdata->ElectronTemp;
         double MassRatio = Pdata->mi/Me;
-        double DebyeLength=sqrt((epsilon0*Kb*Pdata->ElectronTemp)/
-            (Pdata->ElectronDensity*pow(echarge,2)));
-        double DebyeLength_Tilde = DebyeLength/Sample->get_radius();
-        double Betai=Sample->get_radius()/(sqrt(2.0*Kb*Pdata->IonTemp/
-            (PI*Pdata->mi))/(echarge*Pdata->MagneticField.mag3()/Pdata->mi));
-        double Betae=Betai*sqrt(TiTe*MassRatio);
+        double Betae=Sample->get_radius()/(sqrt(Kb*Pdata->ElectronTemp/
+            Me)/(echarge*Pdata->MagneticField.mag3()/Me));
 
-        double a = 1.522;
-        double b = -0.9321;
-        double c = 0.3333;
-        double d = 1.641;
-
-        double TDR=erf(a*pow(DebyeLength,b)*pow(TiTe,c));
-
-        double e = 2.82;
-        double f = 0.4772;
-        double g = 0.1315;
-        double h = 0.7364;
+        double a = 0.2293;
         
-        double Repelled_Species_Current=(exp(-g*Betae)+e*exp(-DebyeLength_Tilde)*
-            pow((Betai/(Betai+1)),h));
-        return -Repelled_Species_Current;
+        double Repelled_Species_Flux=
+		    Pdata->ElectronDensity*sqrt(Kb*Pdata->ElectronTemp/(2*PI*Me))*(1.0+exp(-a*Betae));
+        return -Repelled_Species_Flux;
     }
     double THSi::Evaluate(const Matter* Sample, const std::shared_ptr<PlasmaData> Pdata, const double Potential){
-
         double TiTe = Pdata->IonTemp/Pdata->ElectronTemp;
         double MassRatio = Pdata->mi/Me;
-        double DebyeLength=sqrt((epsilon0*Kb*Pdata->ElectronTemp)/
-            (Pdata->ElectronDensity*pow(echarge,2)));
-        double DebyeLength_Tilde = DebyeLength/Sample->get_radius();
-        double Betai=Sample->get_radius()/(sqrt(2.0*Kb*Pdata->IonTemp/
-            (PI*Pdata->mi))/(echarge*Pdata->MagneticField.mag3()/Pdata->mi));
-        double Betae=Betai*sqrt(TiTe*MassRatio);
+        double Betai=Sample->get_radius()/(sqrt(Kb*Pdata->IonTemp/
+            (Pdata->mi))/(Pdata->Z*echarge*Pdata->MagneticField.mag3()/Pdata->mi));
 
-        double a = 1.522;
-        double b = -0.9321;
-        double c = 0.3333;
-        double d = 1.641;
+        double b = 1.559;
+        double c = 0.5636;
 
-        double TDR=erf(a*pow(DebyeLength,b)*pow(TiTe,c));
+        double Coeff = Pdata->IonDensity*sqrt(Kb*Pdata->IonTemp/(2*PI*Pdata->mi));
+        double Attacted_Species_Flux_T1=Coeff*(1+exp(-b*pow(Betai,c)));
+        double Attacted_Species_Flux_T2=(Coeff*Pdata->Z/TiTe)*2.0*exp(-b*pow(Betai,c));
 
-        double e = 2.82;
-        double f = 0.4772;
-        double g = 0.1315;
-        double h = 0.7364;
+        return Attacted_Species_Flux_T1+Attacted_Species_Flux_T2*Potential;
 
-        double Coeff = Pdata->Z*sqrt(TiTe/MassRatio);
-        double Attacted_Species_Current_T1=Coeff*(exp(-f*Betai)*(TDR+(1-TDR)*d*
-            (1.0/sqrt(TiTe)+1.0/sqrt(DebyeLength_Tilde)))+
-            e*pow((Betai/(Betai+1)),h));
-        double Attacted_Species_Current_T2=(Coeff/TiTe)*exp(-f*Betai)*TDR;
-
-        return Attacted_Species_Current_T1+Attacted_Species_Current_T2*Potential;
     }
     double DTOKSi::Evaluate(const Matter* Sample, const std::shared_ptr<PlasmaData> Pdata, const double Potential){
         return Pdata->Z*Flux::DTOKSIonFlux(Sample,Pdata,Potential);
